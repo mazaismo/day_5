d531b54a327c40f6855cce2ac015ba2d02bfa3e3
Modris Ābrams <brainstormer57@googlemail.com>
2018-05-10 19:40:22 +0300

    2. gatavi

--- day_5/day_5/Program.cs
+++ day_5/day_5/Program.cs
@@ -6,9 +6,32 @@ namespace day_5
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            /* 1. GATAVS
+            Console.WriteLine("Ievadiet lūdzu savu vārdu un uzvārdu");
+            string name;
+            name = Console.ReadLine();
+            Console.WriteLine(name);
             Console.ReadLine();
+            */
 
-        }   
+            Console.WriteLine("Kāds šobrīid ir gads?");
+            int gads = 2;
+            string ievade = Console.ReadLine();
+            
+            gads = Convert.ToInt16(ievade);
+            Console.WriteLine(gads);
+            Console.WriteLine("Kurā gadā jūs piedzimāt");
+            int dz_gads = 9;
+            string ievade2 = Console.ReadLine();
+            dz_gads = Convert.ToInt16(ievade2);
+            Console.Write("tavs vecums ir " );
+            Console.Write(gads - dz_gads);
+
+
+
+            Console.ReadLine();
+        }
+       
+        
     }
 }
